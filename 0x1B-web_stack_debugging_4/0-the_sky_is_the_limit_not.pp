8343d4af271a32370e95064df38b384abca98c8f
Yahya Achrane <yahya.achrane15@gmail.com>
2024-01-25 19:57:34 +0000

    Nginx fix

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+tch up our nginx webserver to handle
+# a large number of requests
+exec { 'fix nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# restart Nginx service (nginx.service)
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
